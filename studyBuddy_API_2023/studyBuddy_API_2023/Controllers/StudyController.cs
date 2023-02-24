272467915d888a6ef093adc4cd22784f3eb7e7f8
Grant Weaver <grantyweaver@gmail.com>
2023-02-23 20:18:25 -0500

    02-23-23

--- studyBuddy_API_2023/studyBuddy_API_2023/Controllers/StudyController.cs
+++ studyBuddy_API_2023/studyBuddy_API_2023/Controllers/StudyController.cs
@@ -12,5 +12,75 @@ namespace studyBuddy_API_2023.Controllers
   {
     StudyQuestionsRepository repo = new StudyQuestionsRepository();
 
+    [HttpPost("add")]
+    public StudyQuestions AddStudyQuestion(StudyQuestions study)
+    {
+      StudyQuestions newStudyQuestion = new StudyQuestions
+      {
+        Id = study.Id,
+        Question = study.Question,
+        Answer = study.Answer
+
+  };
+      return repo.AddStudyQuestion(newStudyQuestion);
+    }
+
+    [HttpGet()]
+    public List<StudyQuestions> GetAll()
+    {
+      return repo.GetAllStudyQuestions();
+    }
+
+    [HttpGet("{id}")]
+    public StudyQuestions GetById(int id)
+    {
+      return repo.FindById(id);
+    }
+
+    [HttpPost("delete/{id}")]
+    public HttpResponseMessage DeleteById(int id)
+    {
+      try
+      {
+        if (repo.DeleteById(id) == true)
+        {
+          return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+        else
+        {
+          return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+      }
+      catch (Exception ex)
+      {
+        return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+      }
+    }
+    [HttpPost("update")]
+    public HttpResponseMessage UpdateStudyQuestions(int id, string question, string answer)
+    {
+      StudyQuestions studyQuestionsToUpdate = new StudyQuestions
+      {
+        Id = id,
+        Question = question,
+        Answer = answer
+      };
+
+      try
+      {
+        if (repo.UpdateStudyQuestions(studyQuestionsToUpdate) == true)
+        {
+          return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+        else
+        {
+          return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+      }
+      catch (Exception ex)
+      {
+        return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+      }
+    }
   }
 }
--- studyBuddy_API_2023/studyBuddy_API_2023/DAL/StudyQuestionsRepository.cs
+++ studyBuddy_API_2023/studyBuddy_API_2023/DAL/StudyQuestionsRepository.cs
@@ -6,5 +6,51 @@ namespace studyBuddy_API_2023.DAL
   public class StudyQuestionsRepository
   {
     private StudyContext _dbContext = new StudyContext();
+
+    public StudyQuestions AddStudyQuestion(StudyQuestions study)
+    {
+      _dbContext.StudyQuestions.Add(study);
+      _dbContext.SaveChanges();
+      return GetLatestStudyQuestions();
+    }
+
+    public List<StudyQuestions> GetAllStudyQuestions()
+    {
+      return _dbContext.StudyQuestions.ToList();
+    }
+
+
+    private StudyQuestions GetLatestStudyQuestions()
+    {
+      return _dbContext.StudyQuestions.OrderByDescending(x => x.Id).FirstOrDefault();
+    }
+
+    public StudyQuestions FindById(int id)
+    {
+      return _dbContext.StudyQuestions.AsNoTracking().FirstOrDefault(x => x.Id == id);
+    }
+
+    public bool DeleteById(int id)
+    {
+      StudyQuestions studyQuestions = FindById(id);
+      if (studyQuestions == null)
+      {
+        return false;
+      }
+      _dbContext.StudyQuestions.Remove(studyQuestions);
+      _dbContext.SaveChanges();
+      return true;
+    }
+    public bool UpdateStudyQuestions(StudyQuestions studyQuestionsToEdit)
+    {
+      if (FindById(studyQuestionsToEdit.Id) == null)
+      {
+        return false;
+      }
+
+      _dbContext.StudyQuestions.Update(studyQuestionsToEdit);
+      _dbContext.SaveChanges();
+      return true;
+    }
   }
 }
